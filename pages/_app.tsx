cbc703c108ee1d8de70ab683f210dec4f9f43845
rsshonjoydas <rsshonjoydas@gmail.com>
2022-01-12 09:27:40 +0600

    project setup

---
+++ pages/_app.tsx
@@ -0,0 +1,4 @@
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }: any) => <Component {...pageProps} />;
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * ! Home Page of the Application
+ * @return {JSX.Element}
+ */
+
+const Home = () => (
+  <div>
+    <h1 className="text-gray-500">rs shonjoy</h1>
+  </div>
+);
+
+export default Home;
